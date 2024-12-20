d589e3fc405497d115c873fd19f2f6a4938a8343
Muhammad Awais Gohar <awaisgohar4560@gmail.com>
2024-12-20 14:52:38 +0500

    Update caraousel.tsx

--- src/app/components/caraousel.tsx
+++ src/app/components/caraousel.tsx
@@ -3,7 +3,7 @@ import women from "@/images/shop-hero-1-product-slide-1 (1).jpg";
 
 export default function Carousel() {
   return (
-    <div className="relative w-full h-auto">
+   <div className="relative w-full h-auto">
   {/* Image for large screens */}
   <div className="block relative w-full h-[400px] sm:h-[500px] md:h-[600px] lg:h-[716px]">
     <Image
@@ -17,7 +17,7 @@ export default function Carousel() {
   </div>
 
   {/* Content Overlay */}
-  <div className="absolute flex flex-col justify-center left-[20px] sm:left-[40px] md:left-[80px] lg:left-[100px] mt-[75px] sm:top-[80px] md:top-[90px] lg:top-[100px] items-start gap-[20px] sm:gap-[25px] md:gap-[30px] lg:gap-[35px] px-4 sm:px-8 lg:px-16 z-10">
+  <div className="absolute flex flex-col justify-center left-[20px] sm:left-[40px] md:left-[80px] lg:left-[100px] top-[50px] sm:top-[80px] md:top-[90px] lg:top-[100px] items-start gap-[20px] sm:gap-[25px] md:gap-[30px] lg:gap-[35px] px-4 sm:px-8 lg:px-16 z-10">
     <h5 className="font-Montserrat font-semibold text-[14px] sm:text-[16px] md:text-[18px] leading-[20px] sm:leading-[24px] text-white">
       SUMMER 2020
     </h5>
